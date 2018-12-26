1104239623e95737f924691f81f330fb202b868f
Manukhurana97 <38296197+Manukhurana97@users.noreply.github.com>
2018-12-26 22:29:34 +0530

    text to speech (incognito)
    text to spech using amazon polly

--- aws/aws polly.py
+++ aws/aws polly.py
@@ -6,8 +6,8 @@ import os
 from pygame import mixer
 
 AB = input("Enter something : ")
-polly_clint = boto3.Session( aws_access_key_id='AKIAI5MVWF2YK5O4EC6Q',
-    aws_secret_access_key='a6wv7VzPM9JpvwsPdz4/iBbINrzFsoucrqZL6q2u',
+polly_clint = boto3.Session( aws_access_key_id=' enter your access_key',
+    aws_secret_access_key='enter yours ',
     region_name='ap-south-1').client('polly') # polly instance
 spoken_text = polly_clint.synthesize_speech(Text=AB, OutputFormat='mp3', VoiceId='Emma')
 
